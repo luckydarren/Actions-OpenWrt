290af20139cb629cfec0aacb5fc733803cf6b16e
luckydarren <63279864+luckydarren@users.noreply.github.com>
2020-07-09 15:53:31 +0800

    Update diy-part1.sh

--- xiaomi-r3g/lean/master/diy-part1.sh
+++ xiaomi-r3g/lean/master/diy-part1.sh
@@ -13,12 +13,12 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Lienol/openwrt-package
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git lienol2 https://github.com/luckydarren/lienol-openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol2 https://github.com/luckydarren/lienol-openwrt-package' feeds.conf.default
 #sed -i '$a src-git lienol2 https://github.com/TinyTitanPro/lienol-openwrt-package.git' feeds.conf.default
 #sed -i '$a src-git lienol2 https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
 
 #Support MIPS FPU EMULATOR
-sed -i '$a CONFIG_GENERIC_PINCONF=y' target/linux/ramips/mt7621/config-4.14
-sed -i '$a CONFIG_GPIOLIB_IRQCHIP=y' target/linux/ramips/mt7621/config-4.14
-sed -i '$a CONFIG_MIPS_FPU_EMULATOR=y' target/linux/ramips/mt7621/config-4.14
-sed -i '/CONFIG_MIPS_CBPF_JIT=y/d' target/linux/ramips/mt7621/config-4.14
+#sed -i '$a CONFIG_GENERIC_PINCONF=y' target/linux/ramips/mt7621/config-4.14
+#sed -i '$a CONFIG_GPIOLIB_IRQCHIP=y' target/linux/ramips/mt7621/config-4.14
+#sed -i '$a CONFIG_MIPS_FPU_EMULATOR=y' target/linux/ramips/mt7621/config-4.14
+#sed -i '/CONFIG_MIPS_CBPF_JIT=y/d' target/linux/ramips/mt7621/config-4.14
